6fa3d857482c7f2a561cd1a85e40c7fcf662bfa3
lorniu <lorniu@gmail.com>
2025-03-08 20:50:47 +0800

    Clean the code and docs.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Bilibili
 
-在 Emacs 中看 B 站。
+在 Emacs 中看 B 站 (https://bilibili.com) 视频。
 
-一些辅助方法（命令）而已。
+一些辅助方法（命令），配合 [mpvi](https://github.com/lorniu/mpvi) 刷视频、学习很方便。
 
 ## 使用步骤
 
- 1. 安装 `mpvi` (https://github.com/lorniu/mpvi)
+ 1. 安装 `mpvi` 包 (`M-x package-install mpvi`)
  2. 下载本包，加入 `load-path`，并通过 `(require 'bilibili)` 加载
  3. 在 org mode 中调用 `bilibili-insert-xxx` 插入视频链接
  4. 点击链接进行视频播放
@@ -25,7 +25,7 @@
 
 - 在某个 Headline 下，第一次执行上述某命令是插入操作，后续执行同一命令则执行更新操作
 - 有些命令 (比如获取推荐视频) 需要 Cookie 支持。在浏览器 (比如 Chrome/Edge) 打开 bilibili.com，按 F12，将 [Network -> Request Header -> cookie] 条目上右键复制的字符串赋值给 `bilibili-cookie-text` 即可
-- 只是用来刷视频，其他额外 API 暂没对接，以后也大概率没兴趣添加
+- 目前只是用来刷视频，其他额外 API 暂没对接
 
 ## 相关链接
 
--- bilibili.el
+++ bilibili.el
@@ -1,10 +1,10 @@
-;;; bilibili.el --- BiliBili in Emacs -*- lexical-binding: t -*-
+;;; bilibili.el --- BiliBili (哔哩哔哩) in org mode -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2023 lorniu <lorniu@gmail.com>
 
 ;; Author: lorniu <lorniu@gmail.com>
 ;; URL: https://github.com/lorniu/bilibili.el
-;; Package-Requires: ((emacs "28.1") (mpvi "1") (org "9.0"))
+;; Package-Requires: ((emacs "29.1") (org "9.0") (mpvi "1.0"))
 ;; Keywords: multimedia, application
 ;; SPDX-License-Identifier: MIT
 ;; Version: 0.9
@@ -24,20 +24,23 @@
 
 ;;; Code:
 
+(require 'cl-lib)
 (require 'org)
 (require 'mpvi)
 
 (defgroup bilibili nil
-  "在 Emacs 中看 B 站."
+  "在 Emacs 中看 B 站。"
   :group 'external
   :prefix 'bilibili-)
 
 (defcustom bilibili-cookie-text nil
-  "在浏览器 (比如 Chrome/Edge) 打开 bilibili，按 F12，在 Network -> Request Header -> cookie 条目上右键复制可得"
+  "需要设定的 B 站的 Cookie 文本。
+在浏览器 (比如 Chrome/Edge) 打开 bilibili，按 F12，在
+Network -> Request Header -> cookie 条目上右键复制可得。"
   :type 'string)
 
 (defcustom bilibili-user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
-  "浏览器的 User Agent"
+  "浏览器的 User Agent 值。"
   :type 'string)
 
 (defvar bilibili-myinfo nil)
@@ -45,6 +48,7 @@
 (defvar url-http-end-of-headers)
 
 (defun bilibili-req (url &optional data headers)
+  "请求方法，携带 DATA 和 HEADERS 发送 URL 到服务器。"
   (let ((url-user-agent bilibili-user-agent)
         (url-request-extra-headers `(("content-type" . "application/x-www-form-urlencoded")
                                      ("cookie" . ,bilibili-cookie-text) ,@headers))
@@ -65,12 +69,14 @@
         (kill-buffer)))))
 
 (defun bilibili-get (url-tpl &rest args)
+  "Get 请求，URL-TPL 和 ARGS 跟 `format' 类似。"
   (let ((resp (bilibili-req (apply #'format url-tpl (mapcar (lambda (i) (if (stringp i) (url-hexify-string i) i)) args)))))
     (when (not (= 0 (alist-get 'code resp)))
       (user-error "ERROR %s: %s" (alist-get 'code resp) (alist-get 'message resp)))
     (alist-get 'data resp)))
 
 (cl-defun bilibili-group-number (str &optional (size 4) (char ","))
+  "格式化 STR 到指定格式。SIZE 和 CHAR 设定了格式化规则。"
   (unless (stringp str)
     (setq str (format "%s" str)))
   (let ((pt (length str)))
@@ -79,23 +85,11 @@
             pt (- pt size)))
     str))
 
-(defun bilibili-string-pixel-width (string)
-  "Return the width of STRING in pixels.
-Compat with `string-pixel-width' in Emacs 29."
-  (if (fboundp 'string-pixel-width) (string-pixel-width string)
-    (if (zerop (length string)) 0
-      (with-current-buffer (get-buffer-create " *string-pixel-width*")
-        (when (bound-and-true-p display-line-numbers-mode)
-          (display-line-numbers-mode -1))
-        (delete-region (point-min) (point-max))
-        (insert string)
-        (car (buffer-text-pixel-size nil nil t))))))
-
 (defun bilibili-get-padding-spaces (text width &optional limit)
-  "Padding TEXT to at least WIDTH with spaces."
-  (let ((w (bilibili-string-pixel-width text)) (num 0))
+  "获取 TEXT 对齐到 WIDTH 所需要的空格字符，最多 LIMIT 个。"
+  (let ((w (string-pixel-width text)) (num 0))
     (while (< w width)
-      (setq w (bilibili-string-pixel-width (concat text (make-string (cl-incf num) ? )))))
+      (setq w (string-pixel-width (concat text (make-string (cl-incf num) ? )))))
     (make-string (max (or limit 0) num) ? )))
 
 
@@ -120,20 +114,21 @@ Compat with `string-pixel-width' in Emacs 29."
    (meta       :initarg :meta)))
 
 (cl-defmethod bilibili-url ((o bilibili-video))
+  "返回视频 O 的 URL 地址。"
   (with-slots (bvid) o
     (format "https://www.bilibili.com/video/%s/" bvid)))
 
 (cl-defmethod bilibili-org-item ((o bilibili-video))
-  "输出到 Org 中的格式。可以根据自己的喜好自定义"
+  "视频 O 输出到 Org 中的格式。可以根据自己的喜好自定义。"
   (with-slots (author mid title played replied duration date meta) o
     (let* ((time (if (numberp duration) (mpvi-secs-to-hms duration nil t) (string-trim duration)))
            (awidth (if-let* ((ps (and author
-                                      (mapcar (lambda (v) (bilibili-string-pixel-width (slot-value v 'author)))
+                                      (mapcar (lambda (v) (string-pixel-width (slot-value v 'author)))
                                               (alist-get 'videos meta)))))
                        (+ (apply #'max ps) 20)
                      220))
            (space1 (if author (bilibili-get-padding-spaces author awidth 1) ""))
-           (twidth (max 1085 (+ (bilibili-string-pixel-width time) awidth 780)))
+           (twidth (max 1085 (+ (string-pixel-width time) awidth 780)))
            (space2 (bilibili-get-padding-spaces (concat author space1 title time) twidth 1)))
       (format "- %s%s· [[%s][%s]]%s%s | %7s | %9s | %s"
               (if author (format "[[https://space.bilibili.com/%s][%s]]" mid author) "")
@@ -146,23 +141,21 @@ Compat with `string-pixel-width' in Emacs 29."
 ;;; APIs
 
 (defun bilibili-get-myinfo ()
-  "我的用户信息"
+  "我的用户信息。"
   (or bilibili-myinfo
       (setq bilibili-myinfo
             (bilibili-get "https://api.bilibili.com/x/space/myinfo"))))
 
 (defun bilibili-get-userinfo (mid)
-  "获取用户详细信息"
-  ;; (bilibili-get-userinfo 2)
+  "获取用户 MID 的详细信息。"
   (bilibili-get "https://api.bilibili.com/x/space/acc/info?mid=%s" mid))
 
 (defun bilibili-get-videoinfo (bvid)
-  "获取视频信息"
+  "获取视频 BVID 的信息。"
   (bilibili-get "https://api.bilibili.com/x/web-interface/view?bvid=%s" bvid))
 
 (cl-defun bilibili-get-followings (&optional mid (order 'attention))
-  "获取所有关注的人"
-  ;; (bilibili-get-followings)
+  "获取所有关注的人，可以通过 MID 和 ORDER 指定用户和排序。"
   (unless mid (setq mid (alist-get 'mid (bilibili-get-myinfo))))
   (unless mid (user-error "MID is required"))
   (let (result total (pn 0))
@@ -179,7 +172,7 @@ Compat with `string-pixel-width' in Emacs 29."
     result))
 
 (cl-defun bilibili-get-popular (&optional (pn 1) (ps 50))
-  "热门视频"
+  "热门视频。可以通过 PN 和 PS 设定数目和页码。"
   (cl-loop with data = (bilibili-get "https://api.bilibili.com/x/web-interface/popular?pn=%d&ps=%d" pn ps)
            for item across (alist-get 'list data)
            for stat = (alist-get 'stat item)
@@ -202,7 +195,7 @@ Compat with `string-pixel-width' in Emacs 29."
                     :shared    (alist-get 'share stat))))
 
 (cl-defun bilibili-get-ranking (&optional (rid 0))
-  "排行榜"
+  "排行榜。RID 表示分区号。"
   (cl-loop with data = (bilibili-get "https://api.bilibili.com/x/web-interface/ranking/v2?rid=%d" rid)
            for item across (alist-get 'list data)
            for stat = (alist-get 'stat item)
@@ -225,7 +218,7 @@ Compat with `string-pixel-width' in Emacs 29."
                     :shared    (alist-get 'share stat))))
 
 (cl-defun bilibili-get-precious (&optional (ps 85))
-  "入站必刷"
+  "入站必刷。PS 表示页数。"
   (cl-loop with data = (bilibili-get "https://api.bilibili.com/x/web-interface/popular/precious?page_size=%d" ps)
            for item across (alist-get 'list data)
            for stat = (alist-get 'stat item)
@@ -248,7 +241,7 @@ Compat with `string-pixel-width' in Emacs 29."
                     :shared    (alist-get 'share stat))))
 
 (cl-defun bilibili-get-recommend (&optional (size 10))
-  "获取推荐视频"
+  "获取推荐视频。SIZE 表示数目。"
   (cl-loop with data = (bilibili-get "https://api.bilibili.com/x/web-interface/index/top/rcmd?version=1&ps=%d" size)
            for item across (alist-get 'item data)
            for stat = (alist-get 'stat item)
@@ -268,21 +261,20 @@ Compat with `string-pixel-width' in Emacs 29."
                     :liked     (alist-get 'like stat))))
 
 (defun bilibili-check-fav (rid)
-  "检查视频是否被收藏"
+  "检查视频是否被 RID 收藏。"
   (eq (alist-get 'favoured (bilibili-get "http://api.bilibili.com/x/v2/fav/video/favoured?aid=%s" rid)) t))
 
 (defun bilibili-get-user-favs (mid)
-  "用户的收藏夹"
+  "用户 MID 的收藏夹。"
   (let ((data (bilibili-get "https://api.bilibili.com/x/v3/fav/folder/created/list-all?up_mid=%s" mid)))
     (cl-coerce (alist-get 'list data) 'list)))
 
 (defun bilibili-get-fav-meta (mlid)
-  "收藏夹详情"
-  ;; mlid = fid+mid[:2]
+  "收藏夹 MLID 的详情。"
   (bilibili-get "https://api.bilibili.com/x/v3/fav/folder/info?media_id=%s" mlid))
 
 (cl-defun bilibili-get-fav-videos (mlid &optional (pn 1) (ps 20))
-  "收藏夹内容"
+  "收藏夹 MLID 的内容。PN 和 PS 是分页参数。"
   (cl-loop with data = (bilibili-get "https://api.bilibili.com/x/v3/fav/resource/list?media_id=%s&pn=%d&ps=%d" mlid pn ps)
            for item across (alist-get 'medias data)
            for author = (alist-get 'upper item)
@@ -304,8 +296,7 @@ Compat with `string-pixel-width' in Emacs 29."
            finally (if (= (length rs) 0) (user-error "Empty content") (cl-return rs))))
 
 (cl-defun bilibili-get-upper-videos (mid &optional (pn 1) (ps 50))
-  "用户发布的视频"
-  ;; (bilibili-get-upper-videos "3985676")
+  "用户 MID 发布的视频。PN 和 PS 是分页参数。"
   (let* ((data (bilibili-get "https://api.bilibili.com/x/space/arc/search?mid=%s&pn=%d&ps=%d" mid pn ps))
          (meta (alist-get 'page data)))
     (when (= 0 (length (alist-get 'vlist (alist-get 'list data))))
@@ -327,8 +318,7 @@ Compat with `string-pixel-width' in Emacs 29."
                       :replied   (alist-get 'comment item)))))
 
 (cl-defun bilibili-get-upper-season-videos (mid sid &optional (pn 1) (ps 30))
-  "用户 MID 创建的合集 SID 的内容"
-  ;; (bilibili-get-upper-season-videos "8047632" "413472")
+  "用户 MID 创建的合集 SID 的内容。PN 代表页码，PS 代表每页数目。"
   (let* ((data (bilibili-get "https://api.bilibili.com/x/polymer/space/seasons_archives_list?mid=%s&season_id=%s&sort_reverse=false&page_num=%d&page_size=%d" mid sid pn ps))
          (items (alist-get 'archives data)))
     (if (= 0 (length items)) (user-error "Empty response"))
@@ -349,7 +339,7 @@ Compat with `string-pixel-width' in Emacs 29."
                         :played    (alist-get 'view (alist-get 'stat item)))))))
 
 (cl-defun bilibili-search-videos (keyword &optional (pn 1))
-  ;; (bilibili-search-video "jinitaimei")
+  "用 KEYWORD 搜索视频，PN 代表编码。"
   (cl-loop with data = (bilibili-get "https://api.bilibili.com/x/web-interface/search/type?search_type=video&keyword=%s&page=%d" keyword pn)
            for item across (alist-get 'result data)
            collect (bilibili-video
@@ -368,7 +358,7 @@ Compat with `string-pixel-width' in Emacs 29."
                     :danmaku   (alist-get 'video_review item))))
 
 (defun bilibili-fav-video (rid mlid)
-  "加入收藏夹. rid is avid or bvid, mlid is media-id, 如果 mlid 为空，那么将是取消收藏"
+  "加入收藏夹. RID is avid or bvid, MLID is media-id, 如果 MLID 为空，那么将是取消收藏。"
   (if (and bilibili-cookie-text (string-match "bili_jct=\\([^;]+\\);" bilibili-cookie-text))
       (let* ((csrf (match-string 1 bilibili-cookie-text))
              (avid (if (string-prefix-p "BV" rid) (alist-get 'aid (bilibili-get-videoinfo rid)) rid))
@@ -384,7 +374,7 @@ Compat with `string-pixel-width' in Emacs 29."
     (user-error "Cookie invalid")))
 
 (defun bilibili-triple-video (rid)
-  "一键三连. rid is avid or bvid"
+  "一键三连. RID is avid or bvid。"
   (if (and bilibili-cookie-text (string-match "bili_jct=\\([^;]+\\);" bilibili-cookie-text))
       (let* ((csrf (match-string 1 bilibili-cookie-text))
              (idstr (format "%s=%s" (if (string-prefix-p "BV" rid) "bvid" "aid") rid))
@@ -398,16 +388,15 @@ Compat with `string-pixel-width' in Emacs 29."
 ;;; Commands
 
 (defmacro bilibili-insert (&rest form)
+  "辅助宏，插入内容到 org buffer, FORM 是宏体。"
   `(if (derived-mode-p 'org-mode)
        (save-excursion
          (dolist (v (progn ,@form))
-           (set-slot-value
-            v 'meta `((videos . ,videos) ,@(slot-value v 'meta)))
            (insert (bilibili-org-item v) "\n")))
      (user-error "只能插入到 org buffer 中")))
 
 (defun bilibili-update-current-org-items (videos)
-  "插入或更新 org buffer 中的结果。这个逻辑有待优化。"
+  "插入或更新 org buffer 中 VIDEOS 的结果。这个逻辑有待优化。"
   (unless (derived-mode-p 'org-mode)
     (user-error "只能插入到 org buffer 中"))
   (save-restriction
@@ -420,8 +409,7 @@ Compat with `string-pixel-width' in Emacs 29."
           (when (search-forward (format "[%s]" (bilibili-url v)) nil t)
             (beginning-of-line)
             (delete-region (line-beginning-position) (+ 1 (line-end-position))))
-          (set-slot-value
-           v 'meta `((videos . ,videos) ,@(ignore-errors (slot-value v 'meta))))
+          (setf (slot-value v 'meta) `((videos . ,videos) ,@(ignore-errors (slot-value v 'meta))))
           (setq rs (concat rs (bilibili-org-item v) "\n")))
         (goto-char (point-min))
         (re-search-forward "^$" nil t)
@@ -430,7 +418,7 @@ Compat with `string-pixel-width' in Emacs 29."
         (if (looking-at-p "[+-] ") (insert d))))))
 
 (defun bilibili-pick-mid ()
-  "读取一个 MID 或选择一个关注的人"
+  "读取一个 MID 或选择一个关注的人。"
   (let* ((all (cons (bilibili-get-myinfo) (bilibili-get-followings)))
          (items (mapcar (lambda (f) (cons (alist-get 'name f) (alist-get 'mid f))) all))
          (choosen (completing-read "填入 UP 的 mid，或从列表中选择: "
@@ -445,7 +433,7 @@ Compat with `string-pixel-width' in Emacs 29."
       (cl-find-if (lambda (f) (equal (alist-get 'mid f) mid)) all))))
 
 (defun bilibili-pick-rid ()
-  "读取视频的 id, avid 或 bvid, 播放中的优先、光标下的其次"
+  "读取视频的 id, avid 或 bvid, 播放中的优先、光标下的其次。"
   (let* ((node (if (derived-mode-p 'org-mode) (cadr (org-element-context))))
          (path (mpvi-prop 'path)) mid)
     (when (and (null path) (equal "https" (plist-get node :type)))
@@ -457,7 +445,7 @@ Compat with `string-pixel-width' in Emacs 29."
         (user-error "好像输入的不是正确的 aid 或 BVID")))))
 
 (defun bilibili-pick-fav (&optional mid)
-  "选中一个收藏夹"
+  "选中一个收藏夹 MID。"
   (unless mid (setq mid (alist-get 'mid (bilibili-get-myinfo))))
   (unless mid (user-error "MID 不能为空，如果选择自己的收藏夹，请确保 `bilibili-cookie-text' 是正确的"))
   (let* ((favs (bilibili-get-user-favs mid))
@@ -476,36 +464,36 @@ Compat with `string-pixel-width' in Emacs 29."
 
 ;;;###autoload
 (defun bilibili-insert-popular (&optional pn)
-  "热门视频"
-  (interactive (list (read-number "Page Number: " 1)))
+  "热门视频，PN 表示页码。"
+  (interactive (list (read-number "Page Number: " 1)) org-mode)
   (bilibili-update-current-org-items (bilibili-get-popular (or pn 1))))
 
 ;;;###autoload
 (defun bilibili-insert-ranking (&optional rid)
-  "排行榜"
-  (interactive (list (read-number "分区号: " 0)))
+  "排行榜，RID 表示分区号。"
+  (interactive (list (read-number "分区号: " 0)) org-mode)
   (bilibili-update-current-org-items (bilibili-get-ranking (or rid 0))))
 
 ;;;###autoload
 (defun bilibili-insert-precious ()
-  "入站必刷视频"
-  (interactive)
+  "入站必刷视频。"
+  (interactive nil org-mode)
   (bilibili-update-current-org-items (bilibili-get-precious)))
 
 ;;;###autoload
 (defun bilibili-insert-recommend ()
-  "获取 10 条推荐视频"
-  (interactive)
+  "获取 10 条推荐视频。"
+  (interactive nil org-mode)
   (bilibili-update-current-org-items (bilibili-get-recommend)))
 
 ;;;###autoload
 (defun bilibili-insert-upper-videos (mid pn)
-  "某个 UP 主的所有视频。分页，PN 表示页码，0 表示所有"
-  (interactive
-   (list (if-let* ((id (org-entry-get (point) "MID")))
-             (read-string "mid: " id nil id)
-           (format "%s" (alist-get 'mid (bilibili-pick-mid))))
-         (read-number "Page Number, 0 for all: " 1)))
+  "UP 主 MID 的所有视频。分页，PN 表示页码，0 表示所有。"
+  (interactive (list (if-let* ((id (org-entry-get (point) "MID")))
+                         (read-string "mid: " id nil id)
+                       (format "%s" (alist-get 'mid (bilibili-pick-mid))))
+                     (read-number "Page Number, 0 for all: " 1))
+               org-mode)
   (cl-assert (and (string-match-p "^[0-9]+$" mid) (>= pn 0)))
   (let ((vs (if (> pn 0)
                 (bilibili-get-upper-videos mid pn)
@@ -526,14 +514,14 @@ Compat with `string-pixel-width' in Emacs 29."
 
 ;;;###autoload
 (defun bilibili-insert-upper-season-videos (mid sid)
-  "某个 UP 主某个合集 SID 中的所有视频"
-  (interactive
-   (list (if-let* ((id (org-entry-get (point) "MID")))
-             (read-string "mid: " id nil id)
-           (format "%s" (alist-get 'mid (bilibili-pick-mid))))
-         (if-let* ((id (org-entry-get (point) "SID")))
-             (read-string "season id: " id nil id)
-           (read-string "season id: ")))) ; 没找到合适的列出合集列表的 API
+  "UP 主 MID 的某个合集 SID 中的所有视频。"
+  (interactive (list (if-let* ((id (org-entry-get (point) "MID")))
+                         (read-string "mid: " id nil id)
+                       (format "%s" (alist-get 'mid (bilibili-pick-mid))))
+                     (if-let* ((id (org-entry-get (point) "SID")))
+                         (read-string "season id: " id nil id)
+                       (read-string "season id: ")))
+               org-mode) ; 没找到合适的列出合集列表的 API
   (cl-assert (and (string-match-p "^[0-9]+$" mid) (string-match-p "^[0-9]+$" mid)))
   (let* ((vs (let (result total (page 0))
                (while (or (null total) (< (length result) total))
@@ -555,7 +543,7 @@ Compat with `string-pixel-width' in Emacs 29."
 
 ;;;###autoload
 (defun bilibili-insert-favs (&optional mlid)
-  "某个收藏夹下的所有视频，MLID 是收藏夹 id"
+  "某个收藏夹下的所有视频，MLID 是收藏夹主键。"
   (interactive (list
                 (if-let* ((id (org-entry-get (point) "MEDIA-ID")))
                     (read-string "media-id of fav: " id nil id)
@@ -575,7 +563,8 @@ Compat with `string-pixel-width' in Emacs 29."
                                                (complete-with-action action pairs input pred)))
                                            nil t)))
                     (org-entry-put (point) "MID" (format "%s" mid))
-                    (cdr (assoc choosen pairs))))))
+                    (cdr (assoc choosen pairs)))))
+               org-mode)
   (let ((meta (bilibili-get-fav-meta mlid)) total (page -1) result)
     (setq total (alist-get 'media_count meta))
     (while (< (length result) total)
@@ -589,9 +578,10 @@ Compat with `string-pixel-width' in Emacs 29."
 
 ;;;###autoload
 (defun bilibili-insert-search (&optional keyword pageno)
-  "搜索符合某个关键词的视频"
+  "搜索符合某个 KEYWORD 的视频。PAGENO 表示页码。"
   (interactive (list (read-string "Keyword to search: ")
-                     (read-number "Page: " 1)))
+                     (read-number "Page: " 1))
+               org-mode)
   (insert (format "Search '%s', page %d:\n" keyword pageno))
   (bilibili-insert (bilibili-search-videos keyword pageno)))
 
@@ -599,7 +589,7 @@ Compat with `string-pixel-width' in Emacs 29."
 
 ;;;###autoload
 (defun bilibili-fav-it ()
-  "加入收藏夹"
+  "加入收藏夹。"
   (interactive)
   (let ((rid (bilibili-pick-rid)))
     (if (bilibili-check-fav rid)
@@ -612,7 +602,7 @@ Compat with `string-pixel-width' in Emacs 29."
 
 ;;;###autoload
 (defun bilibili-triple-it ()
-  "一键三连"
+  "一键三连。"
   (interactive)
   (let ((rid (bilibili-pick-rid)))
     (bilibili-triple-video rid)
@@ -622,6 +612,7 @@ Compat with `string-pixel-width' in Emacs 29."
 ;;; 将 org link 与 `mpvi' 集成。直接点击 bilibili.com 的链接会使用 mpv 打开
 ;; 如果不需要这个，将 bilibili 从 `mpvi-org-https-link-rules' 移除即可
 
+;;;###autoload
 (add-to-list 'mpvi-org-https-link-rules "www.bilibili.com/")
 
 (provide 'bilibili)
